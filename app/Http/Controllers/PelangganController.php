6f26a400a064c0de1a25cba043aba7b67ad58051
nadyaamalya <nadya01mei@gmail.com>
2024-02-24 21:00:10 +0700

    baru

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -1,13 +1,87 @@
 <?php
 
 namespace App\Http\Controllers;
+use RealRashid\SweetAlert\Facades\Alert;
+
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PelangganController extends Controller
 {
     public function index()
     {
-        return view('pages.pelanggan');
+        //mengambil data dari tabel profile
+        $profile = DB::table('profile')->get();
+
+
+
+        $title = 'peringatan !';
+        $text = "Apakah anda yakin ingin menghapus ?";
+        $icon = "Question";
+        confirmDelete($title, $text);
+
+        //mengirim data profile ke view index
+        return view('pelanggan.indexpelanggan', compact('profile'));
+    }
+    
+    public function tambahpelanggan()
+    {
+        return view('pelanggan.tambahpelanggan');
+    }
+
+    public function pelanggan(Request $request){
+
+        $request->validate([
+            'nama' => 'required|',
+            'nohp' => 'required|',
+            'alamat' => 'required|',
+        ]);
+
+        DB::table('profile')-> insert([
+            'nama_lengkap' => $request->nama,
+            'no_hp' => $request->nohp,
+            'alamat' => $request->alamat,
+        ]);
+
+        Alert::success('Success Title', 'Success Message');
+
+        return redirect('/pelanggan'); 
+    }
+
+    public function show($id){
+        $profile = DB::table('profile')->find($id);
+        return view('pelanggan.detailpelanggan', compact('profile'));
+    }
+
+    public function edit($id) {
+        $profile = DB::table('profile')->find($id);
+        return view('pelanggan.editpelanggan', compact('profile'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required|',
+            'nohp' => 'required|',
+            'alamat' => 'required|',
+        ]);
+
+        $request = DB::table('profile')
+            ->where('id', $id)
+            ->update([ //kolom yang mau diambil
+                'nama_lengkap' => $request->nama,
+                'no_hp' => $request->nohp,
+                'alamat' => $request->alamat,
+            ]);
+
+        Alert::success('Success', 'Data Berhasil di Update');
+        return redirect('/pelanggan');
+    }
+    
+    public function destroy($id) {
+        $profile = DB::table('profile')->where('id', $id)->delete();
+        Alert::success('Success', 'Data Berhasil di Hapus');
+        return redirect('/pelanggan');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+//route pelanggan
+Route::get('/dashboard', [DashboardController::class, 'index']); 
 Route::get('/pelanggan', [PelangganController::class, 'index']);
+
+Route::get('/pelanggan', [PelangganController::class, 'index']); 
+Route::get('/tambahpelanggan', [PelangganController::class, 'tambahpelanggan']);
+Route::post('/pelanggan', [PelangganController::class, 'pelanggan']); 
+
+Route::get('/pelanggan/{pelanggan_id}',[PelangganController::class, 'show']); //untuk menampilkan data berdasarkan id tertentu
+Route::get('/pelanggan/{pelanggan_id}/edit',[PelangganController::class, 'edit']);
+Route::put('/pelanggan/{pelanggan_id}',[PelangganController::class, 'update']);
+Route::delete('/pelanggan/{pelanggan_id}',[PelangganController::class, 'destroy']); //untuk mengupdate data berdasarkan id tertentu
